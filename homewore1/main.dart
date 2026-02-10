77328898396090d86b9d051fe06d31359750d61e
abdulrhman <msehelfg@gmail.com>
2026-02-10 14:48:30 +0300

    Create main.dart

---
+++ homewore1/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'عبدالرحمن محمد الاسمري\nرقمي التدريبي: 444239292',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
